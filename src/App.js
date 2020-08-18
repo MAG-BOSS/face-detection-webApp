3cfc322399f3ff74cc0d863fdd3ca675cb528c2c
MAG-BOSS <b18025@students.iitmandi.ac.in>
2020-08-18 16:03:56 +0530

    version1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from "react";
+import Clarifai from "clarifai";
 import './App.css';
+import ImageSearchForm from "./Components/ImageSearchForm/ImageSearchForm.js";
+import FaceDetect from "./Components/FaceDetect/FaceDetect";
+const app=new Clarifai.App(
+  {apiKey: "1e8dcae33bb84884b9ae442a26ed0bce"}
+);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      input:"",
+      imageUrl:"",
+      box:{}
+    };
+  }
+  calculateFacelocation= (data)=>{
+    const clarifaiFace=data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image=document.getElementById("inputimage");
+    const width=Number(image.width);
+    const height =Number(image.height);
+    return{
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width- clarifaiFace.right_col *width,
+      bottomRow: height- clarifaiFace.bottom_row*height
+    };
+  };
+  displayFaceBox=(box)=>{
+    this.setState({box:box});
+  };
+
+  onInputChange=(event)=>{
+    this.setState({input:event.target.value});
+  };
+  onSubmit=()=>{
+    this.setState({imageUrl:this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL,this.state.input).then(
+      function(response){
+        console.log(response);
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err){
+
+      }
+    );
+
+  };
+  render(){
+    return(
+      <div className="App">
+        <ImageSearchForm
+        onInputChange={this.onInputChange}
+        onSubmit={this.onSubmit}
+        />
+        <FaceDetect box={this.state.box} imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
